8e93b1eb7efacea5c8197f319abfdc41d8571fb3
Richard Poroszlay <richardporoszlay@gmail.com>
2024-11-11 12:13:00 +0100

    Implement the operation functionality

--- src/script.js
+++ src/script.js
@@ -13,3 +13,20 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 === 0 || num2 === 0 ? "Error" : num1 / num2;
 }
+
+let operand1;
+let operator;
+let operand2;
+
+function operate(operand1, operator, operand2) {
+    switch(operator) {
+        case '+':
+            return add(operand1, operand2);
+        case '-':
+            return subtract(operand1, operand2);
+        case '*':
+            return multiply(operand1, operand2);
+        case '/':
+            return divide(operand1, operand2);
+    }
+}
